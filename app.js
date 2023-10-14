87b753907d481ca86b349fc0b538213a1ec8da34
pricei <pricei@Xubuntu.myguest.virtualbox.org>
2023-10-14 18:20:56 +1300

    Setup routes and controllers

--- app.js
+++ app.js
@@ -34,6 +34,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
---
+++ controllers/userController.js
@@ -0,0 +1,67 @@
+const User = require("../models/user");
+const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
+
+// Handle sign up on GET
+exports.user_sign_up_get = asyncHandler(async (req, res, next) => {
+  res.render("sign-up-form", { title: "Sign up" });
+});
+
+// Handle sign up on POST.
+exports.user_sign_up_post = [
+  // Validate and sanitize fields.
+  body("first_name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("First name must be specified."),
+  body("last_name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Last name must be specified."),
+  body("username")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Username must be specified."),
+  body("password")
+    .trim()
+    .isLength({ min: 4 })
+    .escape()
+    .withMessage("Password must be at least 4 characters."),
+  body("membership_status").isBoolean().escape(),
+
+  // Process request after validation and sanitization.
+  asyncHandler(async (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create Department object with escaped and trimmed data
+    const user = new User({
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      username: req.body.username,
+      password: req.body.password,
+      membership_status: req.body.membership_status,
+    });
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render form again with sanitized values/errors messages.
+      res.render("sign-up-form", {
+        title: "Sign up",
+        user: user,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      // Data from form is valid.
+
+      // Save department.
+      await user.save();
+      // TODO ******************
+      // Redirect to messages.
+      res.redirect(user.url);
+    }
+  }),
+];
